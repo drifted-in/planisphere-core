bc6974c60940e350db1fd7361b0c83953aefe4d6
jan-tosovsky-cz <jan.tosovsky.cz@gmail.com>
2014-04-14 19:32:08 +0000

    Implementing Firefox workaround for the wheel rotating script.

--- src/main/resources/in/drifted/planisphere/resources/templates/resources/js/mouseEvents.js
+++ src/main/resources/in/drifted/planisphere/resources/templates/resources/js/mouseEvents.js
@@ -3,25 +3,37 @@ var angle = 0;
 var angle0 = 0;
 var angle1 = 0;
 var moving = false;
-var active, passive, wheel;
+var active, passive, wheel, sizeX, sizeY, isFirefox;
 
 function startup(evt) {
+    var userAgent = navigator.userAgent.toLowerCase();
+    isFirefox = /firefox/.test(userAgent);
     svg = evt.target.ownerDocument.documentElement;
     active = svg.getElementById("active");
     passive = svg.getElementById("passive");
     wheel = svg.getElementById("wheel");
+    sizeX = screen.width;
+    sizeY = screen.height;
 }
 
 function wheelMouseDown(evt) {
     moving = true;
-    angle0 = Math.atan2(2 * evt.clientY - svg.clientHeight, 2 * evt.clientX - svg.clientWidth);
+    if (isFirefox) {
+        angle0 = Math.atan2(2 * getY(evt) - sizeY, 2 * getX(evt) - sizeX);
+    } else {
+        angle0 = Math.atan2(2 * evt.clientY - svg.clientHeight, 2 * evt.clientX - svg.clientWidth);
+    }
     passive.setAttribute('style', "cursor: url(../closedhand.cur), crosshair;");
     active.setAttribute('style', "cursor: url(../closedhand.cur), crosshair;");
 }
 
 function wheelMouseMove(evt) {
     if (moving) {
-        angle1 = Math.atan2(2 * evt.clientY - svg.clientHeight, 2 * evt.clientX - svg.clientWidth);
+        if (isFirefox) {
+            angle1 = Math.atan2(2 * getY(evt) - sizeY, 2 * getX(evt) - sizeX);
+        } else {
+            angle1 = Math.atan2(2 * evt.clientY - svg.clientHeight, 2 * evt.clientX - svg.clientWidth);
+        }
         angle = angle + angle1 - angle0;
         angle0 = angle1;
         wheel.setAttribute("transform", "rotate(" + (180 * angle / Math.PI) + ")");
@@ -32,6 +44,30 @@ function wheelMouseUp(evt) {
     moving = false;
     passive.removeAttribute('style');
     active.setAttribute('style', "cursor: url(../openhand.cur), pointer;");
+}
+
+function getX(evt) {
+    var posx = 0;
+    if (evt.pageX) {
+        posx = evt.pageX;
+    }
+    else if (evt.clientX) {
+        posx = evt.clientX + document.body.scrollLeft
+                + document.documentElement.scrollLeft;
+    }
+    return posx;
+}
+
+function getY(evt) {
+    var posy = 0;
+    if (evt.pageY) {
+        posy = evt.pageY;
+    }
+    else if (evt.clientY) {
+        posy = evt.clientY + document.body.scrollTop
+                + document.documentElement.scrollTop;
+    }
+    return posy;
 }
 
 function synchronize(evt) {
